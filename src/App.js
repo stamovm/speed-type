b739cb82cfd680cc80b7404aa09761285d88897f
stamovm <m.stamov@yahoo.com>
2022-03-20 23:31:44 -0700

    Added 5 different text samples in data.js file. Samples are selectable from dropdown menu.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -12,35 +12,29 @@ import {
   Select,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { texts } from './data.js';
 
 function App() {
+  const [selected, setSelected] = useState(0);
+  const [sampleTxt, setSampleTxt] = useState(texts[selected]['txt']);
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Select placeholder="Select text example">
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
+            <Select
+              onChange={e => {
+                setSelected(e.target.value);
+                setSampleTxt(texts[selected]['txt']);
+              }}
+              placeholder="Select text example"
+            >
+              {texts.map(text => (
+                <option value={text.id}>{text.name}</option>
+              ))}
             </Select>
-            <Text>
-              this is a simple paragraph that is meant to be nice and easy to
-              type which is why there will be mommas no periods or any capital
-              letters so i guess this means that it cannot really be considered
-              a paragraph but just a series of run on sentences this should help
-              you get faster at typing as im trying not to use too many
-              difficult words in it although i think that i might start making
-              it hard by including some more difficult letters I am typing
-              pretty quickly so forgive me for any mistakes i think that i will
-              not just tell you a story about the time i went to the zoo and
-              found a monkey and a fox playing together they were so cute and i
-              think that they were not supposed to be in the same cage but they
-              somehow were and i loved watching them horse around forgive the
-              pun well i hope that it has been highly enjoyable typing this
-              paragraph and i wish you the best of luck getting the best score
-              that you possibly can
-            </Text>
+            <Text className="text-sample">{sampleTxt}</Text>
             <Editable defaultValue="Start typing here">
               <EditablePreview />
               <EditableTextarea />
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const texts = [
+  {
+    id: 0,
+    name: 'sample 1 Level: simple',
+    txt: `this is a simple paragraph that is meant to be nice and easy to
+        type which is why there will be mommas no periods or any capital
+        letters so i guess this means that it cannot really be considered
+        a paragraph but just a series of run on sentences this should help
+        you get faster at typing as im trying not to use too many
+        difficult words in it although i think that i might start making
+        it hard by including some more difficult letters I am typing
+        pretty quickly so forgive me for any mistakes i think that i will
+        not just tell you a story about the time i went to the zoo and
+        found a monkey and a fox playing together they were so cute and i
+        think that they were not supposed to be in the same cage but they
+        somehow were and i loved watching them horse around forgive the
+        pun well i hope that it has been highly enjoyable typing this
+        paragraph and i wish you the best of luck getting the best score
+        that you possibly can`,
+  },
+
+  {
+    id: 1,
+    name: 'sample 2 Level:1.9',
+    txt: `Being human makes us susceptible to the onset of feelings. The role of these emotions varies. Some of them are useful while others may be harmful. The use of social media for self-expression has reached a point that it makes us feel we can say anything. This begins when we see people expressing anything and everything that come to mind. When we see everyone else voicing their likes and dislikes, their irritations and desires we tend to imitate what they do. And because many engage in this, we think that it is normal and healthy. However, when we get used to unbridled self-expression, we come to believe that all feelings are valid. We become convinced that in real life, we should also act on our emotions and our impulses. Using social media this way erodes our ability to regulate our actions and reactions. To illustrate, when something small irritates us we think that it's okay to feel this way. But isn't it better to foster one's patience and resilience instead of immediately complaining? Or when we develop an attraction to someone despite that person being in a relationship, and because social media has conditioned us that all feelings can be expressed, we tend to think that acting on this attraction is okay. Not all feelings deserve expression. We find ourselves creating our own problems when we let our present emotions control our actions.`,
+  },
+  {
+    id: 2,
+    name: 'sample 3 Level: 2.3',
+    txt: `Self-confidence is a tricky subject for many people. For some, it's impossible to feel good about themselves without outside validation. When you're in a situation where the people in your life aren't helping you to feel better about yourself, this can become a problem in your day to day life. Most insecurity stems from feelings of not being attractive or feelings of loneliness. If your insecurity doesn't necessarily stem from a lack of interaction, but more a lack of feeling attractive, there are other options that will help you online. Sometimes the best way to put your insecurities to rest can simply be to get an honest opinion. There are multiple support groups online where you can share a picture of yourself with other members and they will give honest feedback on your appearance. In most cases, they will point out good qualities that you may have missed in yourself. But you can trust them to be honest and many members give very valuable style and posture advice to increase your attractiveness. These practical tips and unbiased opinions from supportive strangers will immediately help you feel better about yourself, and if the tips are implemented it will also improve your self-esteem in the long-run.`,
+  },
+  {
+    id: 3,
+    name: 'sample 3 Level: 3.1',
+    txt: `One study examining 30 subjects, of varying different styles and expertise, has found minimal difference in typing speed between touch typists and self-taught hybrid typists. According to the study, "The number of fingers does not determine typing speed... People using self-taught typing strategies were found to be as fast as trained typists... instead of the number of fingers, there are other factors that predict typing speed... fast typists... keep their hands fixed on one position, instead of moving them over the keyboard, and more consistently use the same finger to type a certain letter." To quote doctoral candidate Anna Feit: "We were surprised to observe that people who took a typing course, performed at similar average speed and accuracy, as those that taught typing to themselves and only used 6 fingers on average"`,
+  },
+  {
+    id: 4,
+    name: 'sample 5 Level: 3.7',
+    txt: `Word processors evolved dramatically once they became software programs rather than dedicated machines. They can usefully be distinguished from text editors, the category of software they evolved from. Word processing added to the text editor the ability to control type style and size, to manage lines (word wrap), to format documents into pages, and to number pages. Functions now taken for granted were added incrementally, sometimes by purchase of independent providers of add-on programs. Spell checking, grammar checking and mail merge were some of the most popular add-ons for early word processors. Word processors are also capable of hyphenation, and the management and correct positioning of footnotes and endnotes. Later desktop publishing programs were specifically designed with elaborate pre-formatted layouts for publication, offering only limited options for changing the layout, while allowing users to import text that was written using a text editor or word processor, or type the text in themselves.`,
+  },
+  {
+    id: 5,
+    name: 'sample 6 Level: 4.8',
+    txt: `Business meetings, and professional recordings can contain sensitive data, so security is something a transcription company should not overlook when providing services. Companies should therefore follow the various laws and industry best practice, especially so when serving law firms, government agencies or courts. Medical Transcription specifically is governed by HIPAA, which elaborates data security practices and compliance measures to be strictly followed, failure of which leads to legal action and penalties. Transcription security includes maintaining confidentiality of the data through information security practices including limiting access with passwords and ensuring a secure environment for data and appropriate methods of disposal of all materials and deletion of files. Personnel may be required to sign non-disclosure agreements on a regular basis as well as take various oaths regarding confidentiality and accuracy.`,
+  },
+];
