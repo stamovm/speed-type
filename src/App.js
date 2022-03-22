6e254e788f5dd45791c46d60ae02668312115d3d
stamovm <m.stamov@yahoo.com>
2022-03-21 21:15:07 -0700

    Added  time and wpm counters.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import {
   EditableTextarea,
   EditablePreview,
   Select,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { texts } from './data.js';
@@ -17,25 +23,60 @@ import { texts } from './data.js';
 function App() {
   const [selected, setSelected] = useState(0);
   const [sampleTxt, setSampleTxt] = useState(texts[selected]['txt']);
+  const [time, setTime] = useState(0);
+  const [wpm, setWpm] = useState(0);
+
+  function textChanged(txt) {
+    console.log(txt);
+  }
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Box fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
+          <VStack spacing={5}>
+            <Box minW="100%">
+              <StatGroup>
+                <Stat>
+                  <StatLabel>Time</StatLabel>
+                  <StatNumber>{time}</StatNumber>
+                  <StatHelpText>
+                    <StatArrow type="increase" />
+                    0.0%
+                  </StatHelpText>
+                </Stat>
+
+                <Stat>
+                  <StatLabel>WPM</StatLabel>
+                  <StatNumber>{wpm}</StatNumber>
+                  <StatHelpText>
+                    <StatArrow type="decrease" />
+                    0.0%
+                  </StatHelpText>
+                </Stat>
+                <ColorModeSwitcher joustifySelf="flex-end" />
+              </StatGroup>
+            </Box>
             <Select
               onChange={e => {
                 setSelected(e.target.value);
-                setSampleTxt(texts[selected]['txt']);
+                setSampleTxt(texts[e.target.value]['txt']);
               }}
-              placeholder="Select text example"
             >
               {texts.map(text => (
-                <option value={text.id}>{text.name}</option>
+                <option key={text.id} value={text.id}>
+                  {text.name}
+                </option>
               ))}
             </Select>
             <Text className="text-sample">{sampleTxt}</Text>
-            <Editable defaultValue="Start typing here">
+            <Editable
+              minW="100%"
+              placeholder="Start typing here"
+              onChange={txt => {
+                textChanged(txt);
+              }}
+            >
               <EditablePreview />
               <EditableTextarea />
             </Editable>
