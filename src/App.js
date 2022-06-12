fda840078bc27f06e6ec1f1a7865a5a549ec469d
stamovm <m.stamov@yahoo.com>
2022-06-11 21:09:12 -0700

    Added error count and text marking.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -6,9 +6,7 @@ import {
   VStack,
   Grid,
   theme,
-  Editable,
-  EditableTextarea,
-  EditablePreview,
+  Textarea,
   Select,
   Stat,
   StatLabel,
@@ -27,32 +25,52 @@ import useTimer from './hooks/useTimer';
 function App() {
   const [selected, setSelected] = useState(0);
   const [sampleTxt, setSampleTxt] = useState(texts[selected]['txt']);
+  const [markedTxt, setMarkedTxt] = useState(sampleTxt);
   const [wpm, setWpm] = useState(0);
+  const [errors, setErrors] = useState(0);
   const [progress, setProgress] = useState(0);
+  const inputEl = useRef(null);
 
   const t1 = useTimer(0);
 
   function countWords(words) {
-    //todo: test the new implementation
     const arr = words.split(' ');
     return arr.filter(word => word !== '').length;
   }
 
-  function textChanged(txt) {
-    if (txt.length > 0) {
-      t1.timerStart();
-    } else {
-      t1.timerPause();
+  function markText(txt, words) {
+    let tmpTxt = '';
+    let errCount = 0;
+    for (let i = 0; i < sampleTxt.length; i++) {
+      if (sampleTxt[i] === txt[i]) {
+        tmpTxt += `<span class="correct">${sampleTxt[i]}</span>`;
+      } else {
+        if (txt[i] !== undefined) {
+          tmpTxt += `<span class="wrong">${sampleTxt[i]}</span>`;
+          errCount++;
+        } else {
+          tmpTxt += sampleTxt[i];
+        }
+      }
     }
-    console.log('count: ', (countWords(txt) / countWords(sampleTxt)) * 100);
+    console.log('--', words);
+    setErrors(errCount);
+    setMarkedTxt(tmpTxt);
+  }
+
+  function textChanged(txt) {
     let currentWpm = 0;
-    currentWpm = Math.round(countWords(txt) / (t1.timer / 60));
+    let typedWords = countWords(txt);
+    let totalWords = countWords(sampleTxt);
+    currentWpm = Math.round(typedWords / (t1.timer / 60));
     setWpm(currentWpm);
-    setProgress((countWords(txt) / countWords(sampleTxt)) * 100);
+    setProgress((typedWords / totalWords) * 100);
+    markText(txt, typedWords);
   }
 
   function handleStart() {
     t1.timerStart();
+    setTimeout(() => inputEl.current.focus(), 100);
   }
 
   function handlePause() {
@@ -61,10 +79,13 @@ function App() {
 
   function handleResume() {
     t1.timerResume();
+    setTimeout(() => inputEl.current.focus(), 100);
   }
 
   function handleReset() {
     t1.timerReset();
+    setWpm(0);
+    inputEl.current.value = '';
   }
 
   return (
@@ -79,65 +100,75 @@ function App() {
                   <StatLabel>Time</StatLabel>
                   <StatNumber>{t1.formatTime()}</StatNumber>
                 </Stat>
-
                 <Stat>
                   <StatLabel>WPM</StatLabel>
                   <StatNumber>{wpm}</StatNumber>
                 </Stat>
+                <Stat>
+                  <StatLabel>Errors</StatLabel>
+                  <StatNumber>{errors}</StatNumber>
+                </Stat>
+                <Select
+                  maxW="14rem"
+                  onChange={e => {
+                    setSelected(e.target.value);
+                    setSampleTxt(texts[e.target.value]['txt']);
+                    setMarkedTxt(texts[e.target.value]['txt']);
+                  }}
+                >
+                  {texts.map(text => (
+                    <option key={text.id} value={text.id}>
+                      {text.name}
+                    </option>
+                  ))}
+                </Select>
                 <ColorModeSwitcher />
               </StatGroup>
               <Progress value={progress} />
             </Box>
-            <Select
-              onChange={e => {
-                setSelected(e.target.value);
-                setSampleTxt(texts[e.target.value]['txt']);
+
+            <Text
+              className="text-sample"
+              dangerouslySetInnerHTML={{
+                __html: markedTxt,
               }}
-            >
-              {texts.map(text => (
-                <option key={text.id} value={text.id}>
-                  {text.name}
-                </option>
-              ))}
-            </Select>
-            <Text className="text-sample">{sampleTxt}</Text>
-            <Editable
-              minW="100%"
-              startWithEditView={true}
+            ></Text>
+
+            <Textarea
+              ref={inputEl}
+              minH="5rem"
               placeholder="Start typing here"
-              onChange={txt => {
-                textChanged(txt);
+              isDisabled={t1.isPaused || !t1.isActive}
+              onChange={e => {
+                textChanged(e.target.value);
               }}
-            >
-              <EditablePreview />
-              <EditableTextarea />
-            </Editable>
+            />
             <Stack direction="row" spacing={4} align="left" bgcolor="grey">
               <Button
                 variant="solid"
                 isDisabled={t1.isActive}
-                onClick={() => handleStart()}
+                onClick={handleStart}
               >
                 Start
               </Button>
               <Button
                 variant="solid"
                 isDisabled={t1.isPaused || !t1.isActive}
-                onClick={() => handlePause()}
+                onClick={handlePause}
               >
                 Pause
               </Button>
               <Button
                 variant="solid"
                 isDisabled={!t1.isPaused || !t1.isActive}
-                onClick={() => handleResume()}
+                onClick={handleResume}
               >
                 Resume
               </Button>
               <Button
                 variant="solid"
                 isDisabled={t1.timer === 0}
-                onClick={() => handleReset()}
+                onClick={handleReset}
               >
                 Reset
               </Button>
--- src/data.js
+++ src/data.js
@@ -2,21 +2,7 @@ export const texts = [
   {
     id: 0,
     name: 'sample 1 Difficulty: 0.8',
-    txt: `this is a simple paragraph that is meant to be nice and easy to
-        type which is why there will be no periods or any capital
-        letters so i guess this means that it cannot really be considered
-        a paragraph but just a series of run on sentences this should help
-        you get faster at typing as im trying not to use too many
-        difficult words in it although i think that i might start making
-        it hard by including some more difficult letters I am typing
-        pretty quickly so forgive me for any mistakes i think that i will
-        not just tell you a story about the time i went to the zoo and
-        found a monkey and a fox playing together they were so cute and i
-        think that they were not supposed to be in the same cage but they
-        somehow were and i loved watching them horse around forgive the
-        pun well i hope that it has been highly enjoyable typing this
-        paragraph and i wish you the best of luck getting the best score
-        that you possibly can`,
+    txt: `this is a simple paragraph that is meant to be nice and easy to type which is why there will be no periods or any capital letters so i guess this means that it cannot really be considered a paragraph but just a series of run on sentences this should help you get faster at typing as im trying not to use too many difficult words in it although i think that i might start making it hard by including some more difficult letters I am typing pretty quickly so forgive me for any mistakes i think that i will not just tell you a story about the time i went to the zoo and found a monkey and a fox playing together they were so cute and i think that they were not supposed to be in the same cage but they somehow were and i loved watching them horse around forgive the pun well i hope that it has been highly enjoyable typing this paragraph and i wish you the best of luck getting the best score that you possibly can`,
   },
   {
     id: 1,
